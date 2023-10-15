41522952425fa8bb9db416e972a01390b769637f
Ismail <141173099+alcode111@users.noreply.github.com>
2023-10-15 09:18:55 +0200

    Add mouseover and mouseleave to the hero-title

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ let theater = document.querySelector('#theater');
 
 let heroDiv = document.querySelector('#hero');
 let siteDiv = document.querySelector('#site');
+
+
+// hero
+
+heroTitle.addEventListener('mouseover', () => {
+    heroDiv.style.backgroundImage = 'url("images/7-panorama.jpeg")';
+    heroDiv.style.backgroundSize = '100% 100%';
+})
+
+heroTitle.addEventListener('mouseleave', () => {
+    heroDiv.style.backgroundImage = 'url("images/1-vienne.jpeg")';
+    heroDiv.style.backgroundSize = 'contain';
+})
