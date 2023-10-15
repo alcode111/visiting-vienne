78f3e56074b338bfb7a0c54f870e607bbd0d0c10
Ismail <141173099+alcode111@users.noreply.github.com>
2023-10-15 09:17:15 +0200

    Select all relevant elements

---
+++ index.js
@@ -0,0 +1,9 @@
+let heroTitle = document.querySelector('#hero-title');
+
+let cathedral = document.querySelector('#cathedral');
+let temple = document.querySelector('#temple');
+let theater = document.querySelector('#theater');
+
+
+let heroDiv = document.querySelector('#hero');
+let siteDiv = document.querySelector('#site');
